1bbc3993d6e607facff1ea03e935ead85012cc57
thanh.doi <thanh.doikhac@gmail.com>
2018-07-21 09:45:30 +0000

    top page

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', 'TaskController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::resource('tasks', 'TaskController');
+
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
